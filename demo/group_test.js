8a39627d565d6d8400899aed1389fc71989feae4
BraveWang <1146040444@qq.com>
2019-08-17 22:55:22 +0800

    test for motoboat

---
+++ demo/group_test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const awix = require('../motoboat.js');
+
+var ant = new awix();
+
+ant.config.body_max_size = 600000000;
+//ant.config.log_type = 'stdio';
+ant.config.auto_options = true;
+ant.config.cors = '*';
+//ant.config.show_load_info = true;
+
+var {router, group} = ant;
+
+var api = group('/api');
+
+api.get('/a', async rr => {
+    rr.res.data = {
+        a : 1, b: 2
+    };
+});
+
+api.get('/xyz', async rr => {
+    console.log(rr.group);
+    rr.res.data = 'xyz';
+});
+
+
+api.add(async (rr, next) => {
+    console.log('api say : helo');
+    await next(rr);
+});
+
+ant.add(async (rr, next) => {
+    console.log('global: hey');
+    await next(rr);
+});
+
+router.get('/api/we', async rr => {
+    console.log(rr.group, 'nothing to say');
+    rr.res.data = 'success';
+});
+
+router.options('/*', async rr => {
+    console.log('options');
+});
+
+api.add(async (rr, next) => {
+    console.log('route match : ' + rr.routepath);
+    await next(rr);
+}, /xy/i);
+
+api.get('a/:c/x', async rr => {
+    rr.res.data = rr.args;
+});
+
+router.get('x/y/', async rr => {
+    rr.res.data = `${rr.path}\n${rr.routepath}`;
+});
+
+console.log(ant.api_group_table);
+
+console.log(ant.ApiTable);
+
+console.log(ant.mid_group);
+
+//支持IPv6地址
+ant.run(8098, '::');
