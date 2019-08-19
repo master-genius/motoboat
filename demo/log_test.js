e549265093a7dd2898a7de8566d284367774a1e8
BraveWang <1146040444@qq.com>
2019-08-19 10:55:21 +0800

    test for moto

--- demo/log_test.js
+++ demo/log_test.js
@@ -5,25 +5,9 @@ var app = new mt();
 app.config.log_type = 'file';
 app.config.log_file = '../tmp/access.log';
 app.config.error_log_file = '../tmp/error.log';
-app.config.global_log = true;
+//app.config.global_log = true;
 
 var {router} = app;
-
-var worker_log = async (rr, next) => {
-    var log_data = {
-        method : rr.method,
-        link    : rr.url.origin,
-        time   : (new Date()).toLocaleString("zh-Hans-CN"),
-        status : 200
-    };
-    await next(rr);
-    log_data.status = rr.res.statusCode;
-    if (process.send && typeof process.send === 'function') {
-        process.send(log_data);
-    }
-};
-
-//app.add(worker_log);
 
 router.get('/', async rr => {
     rr.res.data = 'success';
@@ -44,4 +28,4 @@ if (process.argv.length >= 3 && process.argv[2] == '-d') {
     app.config.daemon = true;
 }
 
-app.daemon(2021);
+app.daemon(2021, 2);
